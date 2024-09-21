70d017622bd39ae8e0773ec069e8b21187e59fb5
jmucak <jmucak22@gmail.com>
2024-09-21 15:38:14 +0200

    add file helper

---
+++ src/helpers/FileHelper.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace jmucak\wpHelpersPack\helpers;
+
+class FileHelper {
+	public static function convert_file_size_to_readable_size(int $size) : string {
+		$base   = log( $size ) / log( 1024 );
+		$suffix = array( '', 'KB', 'MB', 'GB', 'TB' );
+		$f_base = floor( $base );
+
+		return round( pow( 1024, $base - floor( $base ) ), 1 ) . $suffix[ $f_base ];
+	}
+}

69ce6b11ad80dcc6c8a61d75589bb69ede7262ce
jmucak <jmucak22@gmail.com>
2024-09-23 18:04:33 +0200

    remove block callback interface

--- src/interfaces/BlockCallbackInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace jmucak\wpHelpersPack\interfaces;
-
-interface BlockCallbackInterface {
-	public function get_view( array $block, string $content, bool $is_preview = false, int $post_id = 0 ): void;
-}
